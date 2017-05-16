2a99c6be9dcf3e322378c4d54ffc2c679bae3b52
ChristianMicheletto <chri.micheletto@gmail.com>
2017-05-16 11:57:04 +0200

    update Bottone

---
+++ Bottone/app/src/main/java/com/example/studente/bottone/R_L.java
@@ -0,0 +1,13 @@
+package com.example.studente.bottone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class R_L extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_r__l);
+    }
+}
