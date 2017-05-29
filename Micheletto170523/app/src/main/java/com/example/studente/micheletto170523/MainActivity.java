984fda6de755a1f3abc0b8f534103415c7e3e8ac
ChristianMicheletto <chri.micheletto@gmail.com>
2017-05-29 19:19:32 +0200

    Verifica completata

--- Micheletto170523/app/src/main/java/com/example/studente/micheletto170523/MainActivity.java
+++ Micheletto170523/app/src/main/java/com/example/studente/micheletto170523/MainActivity.java
@@ -9,37 +9,35 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     TextView vt;
-
     Button start;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        vt= (TextView) findViewById(R.id.vittoria);
-        start= (Button) findViewById(R.id.startGame);
+        vt = (TextView) findViewById(R.id.vittoria);
+        start = (Button) findViewById(R.id.startGame);
         start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               Intent i=new Intent(MainActivity.this,game.class);
-                startActivity(i);
+                Intent i = new Intent(MainActivity.this, game.class);
+                startActivityForResult(i,10);
 
             }
         });
 
 
-
     }
 
-    public void onActivityResult(int requestCode, int resultCode, Intent i){
-        if(requestCode==10){
-            if(resultCode>10){
+    public void onActivityResult(int requestCode, int resultCode, Intent i) {
+        if (requestCode == 10) {
+            if (resultCode > 10) {
                 vt.setText("Vittoria");
-               vt.setBackgroundColor(getResources().getColor(R.color.GREEN));
+                vt.setTextColor(getResources().getColor(R.color.GREEN));
             }
-            if(resultCode<10) {
+            if (resultCode < 10) {
                 vt.setText("Hai perso");
-                vt.setBackgroundColor(getResources().getColor(R.color.RED));
+                vt.setTextColor(getResources().getColor(R.color.RED));
 
             }
         }
